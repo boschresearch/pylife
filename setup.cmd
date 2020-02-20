377f2ecd380b5bc249542f072aafa51daf328547
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-02-20 14:49:54 +0100

    CREATE-3293: Removed PIP packages from conda install Added coverage report generation

--- setup.cmd
+++ setup.cmd
@@ -4,6 +4,6 @@ call conda activate _venv2
 
 for /F "tokens=*" %%A in (requirements_PIP.txt) do pip install %%A
 
-call pytest
+call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife/ || exit /B 1
 
 call conda deactivate
