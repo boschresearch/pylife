f1b7fbfdba99412caa952fe25293cc9018575210
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-02-21 07:38:55 +0100

    CREATE-3293: fixed default channels
    Signed-off-by: Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,6 @@
-call conda create -y -n _venv2 --file ./requirements_CONDA.txt
+call conda create -y -n _venv --file ./requirements_CONDA.txt
 
-call conda activate _venv2
+call conda activate _venv
 
 for /F "tokens=*" %%A in (requirements_PIP.txt) do pip install %%A
 
