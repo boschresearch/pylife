e701f1df9c4ebed54ae5b66f8a7653d462f7332c
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-02-21 08:11:17 +0100

    CREATE-3293:changed back original create env sh

--- create_environment.sh
+++ create_environment.sh
@@ -12,23 +12,14 @@ fi
 
 python_version=`head -1 requirements.txt`
 
-echo $python_version
-
-conda create -p _venv --file ./requirements_CONDA.txt
-echo "Environment created"
-
+conda create -p _venv \"$python_version\" pip --yes
 conda activate ./_venv
-echo "Environment activated"
 
-cat requirements_PIP.txt | \
+sed '1d' requirements.txt | \
 while read req ; do
-	echo $req
-    pip install $req
+    conda install $req --yes || pip install $req
 done
 
-echo "Pip packages installed"
-
 conda deactivate
-echo "Environment deactivated"
 
 shopt -q login_shell && read -n1 -r -p "Press any key to continue..." key
