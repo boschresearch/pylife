0fe22e2d36e96c625d1c8057e74d759768f70cd0
DE\KRD2RNG <DanielChristopher.Kreuter@de.bosch.com>
2020-03-02 12:46:56 +0100

    LifeTimeCalc finished so far; check the damage calc one again

--- pylife/stress/timesignal.py
+++ pylife/stress/timesignal.py
@@ -109,3 +109,94 @@ class TimeSignalGenerator:
         A resetted generator behaves like a new generator.
         '''
         self.time_position = 0.0
+
+
+class TimeSignalPrep:
+
+    def __init__(self,df):
+
+        self.df = df
+
+    def resample_acc(self,sample_rate_new = 1):
+        """ Resampling the time series
+         Parameters:
+        ----------
+
+        self: DataFrame
+
+        time_col: str
+            column name of the time column
+        sample_rate_new: float
+            sample rate of the resampled time series
+        Returns:
+        --------
+        DataFrame
+        """
+
+#        dfResample.index =  np.arange(self.df.index.min(),self.df.index.max(),1/sample_rate_new)
+        index_new =  np.linspace(self.df.index.min(),
+                      self.df.index.min() + np.floor((self.df.index.max()-self.df.index.min())*sample_rate_new)/sample_rate_new,
+                      (self.df.index.max()-self.df.index.min())*sample_rate_new + 1)
+        dfResample = pd.DataFrame(index = index_new)
+        for colakt in self.df.columns:
+            dfResample[colakt] = np.interp(dfResample.index,self.df.index,self.df[colakt])
+        return dfResample
+
+    def butter_bandpass(self,lowcut, highcut, fs, order=5):
+        """Use the functonality of scipy"""
+        nyq = 0.5 * fs
+        low = lowcut / nyq
+        high = highcut / nyq
+        b, a = signal.butter(order, [low, high], btype='band')
+        TSout = signal.filtfilt(b, a, self.df)
+        return TSout
+    def running_stats_filt(self,col,window_length = 2048,buffer_overlap = 0.1,limit = 0.05, method = "rms"):
+        """
+        Calculates the running statistics of one DataFrame column and drops the rejected data points from the whole DataFrame.
+
+        **Attention**: Reset_index is used
+
+        Parameters:
+        ----------
+
+        self: DataFrame
+
+        col: str
+            column name of the signal for the runnings stats calculation
+        window_length: int
+            window length of the single time snippet, default is 2048
+        buffer_overlap: float
+            overlap parameter, 0.1 is equal to 10 % overlap of every buffer, default is 0.1
+        limit: float
+            limit value of skipping values, 0.05 for example skips all values which buffer method parameter is lower than 5% of the total max value,
+            default is 0.05
+        method: str
+            method: 'rms', 'min', 'max', 'abs', default is 'rms'
+        Returns:
+        --------
+        DataFrame
+
+        """
+        df = self.df.reset_index(drop = True)
+        delta_t = self.df.index.values[1]-self.df.index.values[0]
+        hop = int(window_length*(1-buffer_overlap)) # absolute stepsize
+        df = df.loc[:int(np.floor(len(df)/hop)*hop),:]
+        n_iter = 1+int((len(df)-window_length)/(hop))
+        ind_act = 0
+        stats_list = []
+        for ii in range (n_iter):
+            if method == "rms":
+                stats_list.append( np.sqrt(np.mean(df[col][ind_act:ind_act+window_length]**2)))
+            elif method == "max":
+                stats_list.append(np.max(df[col][ind_act:ind_act+window_length]))
+            elif method == "min":
+                stats_list.append(np.abs(np.min(df[col][ind_act:ind_act+window_length])))
+            elif method == "abs":
+                stats_list.append(np.abs(df[col][ind_act:ind_act+window_length]))
+            ind_act = ind_act+hop
+        stats_list = pd.DataFrame({"stats": np.asarray(stats_list)})
+        stats_list = stats_list[stats_list["stats"] < limit*stats_list["stats"].max()]
+        for ind_act in stats_list.index:
+            df = df.drop(index = np.arange(ind_act*hop,ind_act*hop+window_length), errors = 'ignore')
+        df.index = np.linspace(0,delta_t*(len(df)-1), len(df))
+        return df
