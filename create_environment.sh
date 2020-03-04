ef79a76ba9080c76197e4d6abc9071e276f44652
Johannes Mueller <johannes.mueller4@de.bosch.com>
2020-03-04 14:00:10 +0100

    Make canonical environment from .yml file
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- create_environment.sh
+++ create_environment.sh
@@ -5,20 +5,20 @@ git config filter.jupyter_clean.clean \
     --stdin --stdout --to notebook --ClearOutputPreprocessor.enabled=True"
 
 if [[ `uname` = Linux ]] ; then
-    . ~/miniconda3/etc/profile.d/conda.sh
+    . $ANACONDA_HOME/etc/profile.d/conda.sh
 else
-    eval "`sed '1 s|^.*$|_CONDA_EXE="/c/Program Files/Anaconda3/Scripts/conda.exe"|;s/\$_CONDA_EXE/"$_CONDA_EXE"/g' /c/Program\ Files/Anaconda3/etc/profile.d/conda.sh`"
+    eval $('/c/Program Files/Anaconda3/Scripts/conda.exe' 'shell_bash', 'hook')
 fi
 
 python_version=`head -1 requirements.txt`
 
-conda create -p _venv \"$python_version\" pip --yes
+conda env create -p _venv --file environment.yml
+
 conda activate ./_venv
 
-sed '1d' requirements.txt | \
-while read req ; do
-    conda install $req --yes || pip install $req
-done
+if [[ `uname` != Linux ]] ; then
+    conda install pywin32
+fi
 
 conda deactivate
 
