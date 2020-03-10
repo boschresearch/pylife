8fe29d12ca3bfe8e11c2c9467c5f643c7723d135
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-03-10 14:49:26 +0100

    test
    Signed-off-by: Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>

--- jenkins.cmd
+++ jenkins.cmd
@@ -1,5 +1,3 @@
-call ./create_environment.cmd
-
 call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife/ || exit /B 1
 
 if exist flake8.log del flake8.log
@@ -7,4 +5,3 @@ call flake8 --exit-zero --exclude=virtual-env > flake8.log || exit /B 1
 
 call conda deactivate
 
-call conda env remove --name _pylife
