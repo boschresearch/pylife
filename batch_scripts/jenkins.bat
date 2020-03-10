cc4b999317938ea49e78da7732e536164e6c4c51
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-03-10 15:56:36 +0100

    test

--- batch_scripts/jenkins.bat
+++ batch_scripts/jenkins.bat
@@ -1,6 +1,6 @@
 call conda activate _pylife
 
-call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife/ || exit /B 1
+call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=../pylife/ || exit /B 1
 
 if exist flake8.log del flake8.log
 call flake8 --exit-zero --exclude=virtual-env > flake8.log || exit /B 1
