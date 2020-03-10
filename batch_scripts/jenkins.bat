35b31316803dc498a1a0d10d0fa12d3c9f84e181
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-03-10 16:09:17 +0100

    CREATE-3293: path environment instead of named

--- batch_scripts/jenkins.bat
+++ batch_scripts/jenkins.bat
@@ -1,9 +1,9 @@
-call conda activate _pylife
+call conda activate ./_venv
 
-call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=../pylife/ || exit /B 1
+call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife || exit /B 1
 
 if exist flake8.log del flake8.log
-call flake8 --exit-zero --exclude=virtual-env > flake8.log || exit /B 1
+call flake8 --exit-zero --exclude=_venv > flake8.log || exit /B 1
 
 call conda deactivate
 
