af2d888dd5b83d7e62907c57689191dfc9562ab6
Johannes Mueller <johannes.mueller4@de.bosch.com>
2020-03-13 15:24:40 +0100

    Add test cases for meanstress transformation with R=-inf
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- tests/strength/test_meanstress.py
+++ tests/strength/test_meanstress.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import copy
+import sys, os, copy
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -8,55 +9,80 @@ import numpy.testing as testing
 
 import pylife.strength.meanstress as MST
 
-def create_cyclic_signal(Sm, Sa):
-    return pd.DataFrame( {'sigma_m': Sm, 'sigma_a': Sa } )
 
-def create_series_goodman_signal(M, M2):
-    return pd.Series({ 'M':M, 'M2':M2 })
-
-def create_dataframe_goodman_signal(M, M2, MCount):
-    return pd.DataFrame({ 'M':[M]*MCount, 'M2':[M2]*MCount, })
-
-def test_FKM_goodman_plain_different_m_a():
+def goodman_signal_sm():
     Sm = np.array([-4., -2.,   -1., 0., 0.4, 2./3., 7./6.])
     Sa = np.array([ 2.,  2., 3./2., 1., 0.8, 2./3., 7./12.])
-    goodman_signal = create_series_goodman_signal(0.5, 0.5 / 3)
+    return pd.DataFrame({'sigma_m': Sm, 'sigma_a': Sa })
+
+def goodman_signal_r():
+    Sm = np.array([-4., -2.,   -1., 0., 0.4, 2./3., 7./6.])
+    Sa = np.array([ 2.,  2., 3./2., 1., 0.8, 2./3., 7./12.])
+    warnings.simplefilter('ignore', RuntimeWarning)
+    R = (Sm-Sa)/(Sm+Sa)
+    warnings.simplefilter('default', RuntimeWarning)
+    return pd.DataFrame({'sigma_a': Sa, 'R': R})
+
+
+def test_FKM_goodman_plain_sm():
+    cyclic_signal = goodman_signal_sm()
+    Sa = cyclic_signal.sigma_a.to_numpy()
+    Sm = cyclic_signal.sigma_m.to_numpy()
+    M = 0.5
+
+    R_goal = 1.
+    testing.assert_raises(ValueError, MST.FKM_goodman, Sa, Sm, M, M/3, R_goal)
+
     R_goal = -1.
+    res = MST.FKM_goodman(Sa, Sm, M, M/3, R_goal)
+    np.testing.assert_array_equal(res, np.ones_like(res))
 
-    res = goodman_signal.FKM_Goodman.FKM_goodman(Sa, Sm, R_goal)
-    assert np.array_equal(res, np.ones_like(res))
-
-def test_FKM_goodman_plain_same_m_a():
     Sm = np.array([5])
     Sa = np.array([0])
-    goodman_signal = create_series_goodman_signal(0.5, 0.5 / 3)
-    R_goal = -1.
-
-    res = goodman_signal.FKM_Goodman.FKM_goodman(Sa, Sm, R_goal)
+    res = MST.FKM_goodman(Sa, Sm, M, M/3, R_goal)
     assert np.equal(res,0.)
 
 
-def test_FKM_goodman_single_M():
-    Sm = np.array([-4., -2.,   -1., 0., 0.4, 2./3., 7./6.])
-    Sa = np.array([ 2.,  2., 3./2., 1., 0.8, 2./3., 7./12.])
-    goodman_signal = create_series_goodman_signal(0.5, 0.5 / 3)
-    cyclic_signal = create_cyclic_signal(Sm, Sa)
+
+def test_FKM_goodman_single_M_sm():
+    cyclic_signal = goodman_signal_sm()
+    M = 0.5
+
     R_goal = -1.
 
-    res = cyclic_signal.meanstress_mesh.FKM_goodman(goodman_signal, R_goal).sigma_a
-    assert np.array_equal(res, np.ones_like(res))
+    res = cyclic_signal.meanstress_mesh.FKM_goodman(pd.Series({ 'M':M, 'M2':M/3 }), R_goal).sigma_a
+    np.testing.assert_array_equal(res, np.ones_like(res))
 
-def test_FKM_goodman_multiple_M():
-    Sm = np.array([-4., -2.,   -1., 0., 0.4, 2./3., 7./6.])
-    Sa = np.array([ 2.,  2., 3./2., 1., 0.8, 2./3., 7./12.])   
-    goodman_signal = create_dataframe_goodman_signal(0.5, 0.5 / 3, 7)
-    cyclic_signal = create_cyclic_signal(Sm, Sa)
+
+def test_FKM_goodman_single_M_R():
+    cyclic_signal = goodman_signal_r()
+    M = 0.5
+
     R_goal = -1.
 
-    res = cyclic_signal.meanstress_mesh.FKM_goodman(goodman_signal, R_goal).sigma_a
-    print(res)
-    assert np.array_equal(res, np.ones_like(res))
- 
+    res = cyclic_signal.meanstress_mesh.FKM_goodman(pd.Series({ 'M':M, 'M2':M/3 }), R_goal).sigma_a
+    np.testing.assert_array_equal(res, np.ones_like(res))
+
+
+
+def test_FKM_goodman_multiple_M_sm():
+    cyclic_signal = goodman_signal_sm()
+    M = 0.5
+
+    R_goal = -1.
+    res = cyclic_signal.meanstress_mesh.FKM_goodman(pd.DataFrame({ 'M':[M]*7, 'M2':[M/3]*7, }), R_goal).sigma_a
+    np.testing.assert_array_equal(res, np.ones_like(res))
+
+
+def test_FKM_goodman_multiple_M_sm():
+    cyclic_signal = goodman_signal_r()
+    M = 0.5
+
+    R_goal = -1.
+    res = cyclic_signal.meanstress_mesh.FKM_goodman(pd.DataFrame({ 'M':[M]*7, 'M2':[M/3]*7, }), R_goal).sigma_a
+    np.testing.assert_array_equal(res, np.ones_like(res))
+
+
 def test_5_segment():
     Sm = np.array([-12./5., -2., -1., 0., 2./5., 2./3., 7./6., 1.+23./75., 2.+1./150., 3.+11./25., 3.+142./225.])
     Sa = np.array([ 6./5., 2., 3./2., 1., 4./5., 2./3., 7./12., 14./25., 301./600., 86./225., 43./225.])
