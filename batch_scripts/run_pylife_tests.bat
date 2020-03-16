9f61ed7afb3bfbecf9734f5990a138412abf52b9
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-03-16 14:27:09 +0100

    CREATE-3293: fixed path to coverage home folder
    Signed-off-by: Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>

--- batch_scripts/run_pylife_tests.bat
+++ batch_scripts/run_pylife_tests.bat
@@ -2,4 +2,4 @@ call conda activate ./_venv
 
 call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife || exit /B 1
 
-call python batch_scripts\test_cov_analysis.py E:\pyLife_coverage
+call python batch_scripts\test_cov_analysis.py E:\pyLife_coverage\
