693a0224c1493e4190be9df3df056b66e3d2469a
Gyongyver Kiss <gyongyver.kiss@hu.bosch.com>
2020-10-12 14:49:41 +0200

    - Removed unused req files - Fixed jupyter path for nbconvert - fixed setup.py for cython

--- batch_scripts/create_pylife_environment.bat
+++ batch_scripts/create_pylife_environment.bat
@@ -2,9 +2,9 @@ call conda env remove -p ./_venv
 
 for /f "tokens=*" %%a in ('git rev-parse --show-toplevel') do (set repo_path=%%a)
 
-set jupyter_path=%repo_path%_venv/Scripts/jupyter 
+set jupyter_path=%repo_path%/_venv/Scripts/jupyter
 
-call git config filter.jupyter_clean.clean "$jupyter_path nbconvert --stdin --stdout --to notebook --ClearOutputPreprocessor.enabled=True"
+call git config filter.jupyter_clean.clean "%jupyter_path% nbconvert --stdin --stdout --to notebook --ClearOutputPreprocessor.enabled=True"
 
 call conda env create -p _venv --file environment.yml
 
