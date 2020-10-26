88bdfb802c8e0038cadeae006107d775f11373bb
Johannes Mueller <johannes.mueller4@de.bosch.com>
2020-10-26 16:35:01 +0100

    Remove obsolete coding tags
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- tests/materialdata/woehler/test_analyzer.py
+++ tests/materialdata/woehler/test_analyzer.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -*- coding: utf-8 -*-
 
 import numpy as np
 import pandas as pd
--- tests/strength/test_helpers.py
+++ tests/strength/test_helpers.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -*- coding: utf-8 -*-
 
 from pylife.strength import helpers as hlp
 
