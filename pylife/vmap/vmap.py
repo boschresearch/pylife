213f2fb773428be45d60792a4d5be9e12dce7e72
Johannes Mueller <johannes.mueller4@de.bosch.com>
2020-12-03 17:56:37 +0100

    Drafting a VMAP importer module
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

---
+++ pylife/vmap/vmap.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2019-2020 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository
+# https://github.com/boschresearch/pylife
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Johannes Mueller"
+__maintainer__ = __author__
+
+import numpy as np
+import pandas as pd
+
+import h5py
+
+
+class VMAP:
+    _column_names = {
+        'DISPLACEMENT': ['dx', 'dy', 'dz'],
+        'STRESS_CAUCHY': ['S11', 'S22', 'S33', 'S12', 'S13', 'S23'],
+        'EVOL': ['V_e']
+    }
+
+    def __init__(self, filename):
+        self._file = h5py.File(filename, 'r')
+        self._index_gen = {
+            2: self._var_node_index,
+            3: self._var_element_index,
+            6: self._var_element_nodal_index
+        }
+
+    def nodes(self, geometry):
+        return pd.DataFrame(
+            self._file["/VMAP/GEOMETRY/%s/POINTS/MYCOORDINATES" % geometry][()],
+            columns=['x', 'y', 'z'],
+            index=self._node_index(geometry)
+        )
+
+    def mesh_index(self, geometry):
+        connectivity = self._element_connectivity(geometry).connectivity
+        length = sum([el.shape[0] for el in connectivity])
+        index_np = np.empty((2, length), dtype=np.int64)
+
+        i = 0
+        for eid, nds in connectivity.iteritems():
+            i_next = i + nds.shape[0]
+            index_np[0, i:i_next] = eid
+            index_np[1, i:i_next] = nds
+            i = i_next
+
+        return pd.MultiIndex.from_arrays(index_np, names=['element_id', 'node_id'])
+
+    def mesh_coords(self, geometry):
+        return pd.DataFrame(index=self.mesh_index(geometry)).join(self.nodes(geometry))
+
+    def variable(self, geometry, state, varname):
+        var_tree = self._file["/VMAP/VARIABLES/%s/%s/%s" % (state, geometry, varname)]
+        return pd.DataFrame(
+            data=var_tree['MYVALUES'][()],
+            columns=self._column_names[varname],
+            index=self._make_index(var_tree, geometry)
+        )
+
+    def _element_connectivity(self, geometry):
+        elements = self._file['/VMAP/GEOMETRY/'+geometry+'/ELEMENTS/MYELEMENTS']
+        element_connectivity = elements['myIdentifier', 'myConnectivity'][:, 0]
+        element_ids = [elid for elid, _ in element_connectivity]
+        connectivity = [conn for _, conn in element_connectivity]
+        return pd.DataFrame(data={'element_id': element_ids, 'connectivity': connectivity}).set_index('element_id')
+
+    def _node_index(self, geometry):
+        return pd.Index(
+            self._file["/VMAP/GEOMETRY/%s/POINTS/MYIDENTIFIERS" % geometry][:, 0],
+            name='node_id'
+        )
+
+    def _make_index(self, var_tree, geometry):
+        location = var_tree.attrs['MYLOCATION']
+        return self._index_gen[location](var_tree, geometry)
+
+    def _var_node_index(self, var_tree, _):
+        return pd.Index(var_tree['MYGEOMETRYIDS'][:, 0], name='node_id')
+
+    def _var_element_index(self, var_tree, _):
+        return pd.Index(var_tree['MYGEOMETRYIDS'][:, 0], name='element_id')
+
+    def _var_element_nodal_index(self, var_tree, geometry):
+        mesh_index_frame = self.mesh_index(geometry).to_frame(index=False)
+        index_frame = pd.DataFrame(var_tree['MYGEOMETRYIDS'], columns=['element_id'])
+
+        return (index_frame
+                .merge(mesh_index_frame)
+                .set_index(['element_id', 'node_id'])
+                .index)
---
+++ tests/vmap/test_vmap_import.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+import pylife.vmap as vmap
+
+import reference_data as RD
+
+
+@pytest.fixture
+def beam_2d_squ():
+    return vmap.VMAP('tests/vmap/testfiles/beam_2d_squ_lin.vmap')
+
+
+@pytest.fixture
+def beam_3d_hex():
+    return vmap.VMAP('tests/vmap/testfiles/beam_3d_hex_lin.vmap')
+
+
+def test_get_nodes_2d(beam_2d_squ):
+    pd.testing.assert_frame_equal(beam_2d_squ.nodes('1'), RD.beam_2d_squ_nodes)
+
+
+def test_get_mesh_index(beam_2d_squ):
+    pd.testing.assert_index_equal(beam_2d_squ.mesh_index('1'), RD.beam_2d_squ_mesh_index)
+
+
+def test_get_mesh_coords(beam_2d_squ):
+    pd.testing.assert_frame_equal(beam_2d_squ.mesh_coords('1'), RD.beam_2d_squ_mesh_coords)
+
+
+def test_get_node_variable_displacement(beam_2d_squ):
+    var_frame = beam_2d_squ.variable('1', 'STATE-2', 'DISPLACEMENT')
+    pd.testing.assert_frame_equal(var_frame, RD.beam_2d_squ_node_displacement)
+
+
+def test_get_element_variable_evol(beam_3d_hex):
+    var_frame = beam_3d_hex.variable('1', 'STATE-2', 'EVOL')
+    pd.testing.assert_frame_equal(var_frame, RD.beam_3d_hex_node_displacement)
+
+
+def test_get_element_nodal_variable_stress(beam_2d_squ):
+    var_frame = beam_2d_squ.variable('1', 'STATE-2', 'STRESS_CAUCHY')
+    pd.testing.assert_frame_equal(var_frame, RD.beam_2d_squ_element_nodal_stress)
