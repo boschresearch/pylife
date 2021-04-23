68cc12ef28b31f52f105bc79a896ce78cc068d34
Johannes Mueller <johannes.mueller4@de.bosch.com>
2021-04-23 10:05:02 +0200

    Add missing tests for PylifeSignal
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- tests/core/test_signal.py
+++ tests/core/test_signal.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pytest 
+import pytest
 import pandas as pd
 
 import pylife.core.signal as signal
@@ -23,8 +23,10 @@ from pylife.core.data_validator import DataValidator
 foo_bar_baz = pd.DataFrame({'foo': [1.0], 'bar': [1.0], 'baz': [1.0]})
 val = DataValidator()
 
+
 def test_missing_keys_none():
     assert val.get_missing_keys(foo_bar_baz, ['foo', 'bar']) == []
+
 
 def test_missing_keys_one():
     assert val.get_missing_keys(foo_bar_baz, ['foo', 'foobar']) == ['foobar']
@@ -32,6 +34,7 @@ def test_missing_keys_one():
 
 def test_missing_keys_two():
     assert set(val.get_missing_keys(foo_bar_baz, ['foo', 'foobar', 'barfoo'])) == set(['foobar', 'barfoo'])
+
 
 @pd.api.extensions.register_dataframe_accessor('test_accessor_none')
 class AccessorNone(signal.PylifeSignal):
@@ -44,7 +47,11 @@ class AccessorNone(signal.PylifeSignal):
         validator.fail_if_key_missing(obj, ['foo', 'bar'])
 
     def already_here(self):
-        pass
+        return 23
+
+    @property
+    def some_property(self):
+        return 42
 
 @pd.api.extensions.register_dataframe_accessor('test_accessor_one')
 class AccessorOne:
@@ -56,6 +63,7 @@ class AccessorOne:
     def _validate(self, obj, validator):
         validator.fail_if_key_missing(obj, ['foo', 'foobar'])
 
+
 @pd.api.extensions.register_dataframe_accessor('test_accessor_two')
 class AccessorTwo:
     def __init__(self, pandas_obj):
@@ -66,23 +74,37 @@ class AccessorTwo:
     def _validate(self, obj, validator):
         validator.fail_if_key_missing(obj, ['foo', 'foobar', 'barfoo'])
 
+
 def test_fail_if_missing_keys_none():
     foo_bar_baz.test_accessor_none
+
 
 def test_fail_if_missing_keys_one():
     with pytest.raises(AttributeError, match=r'^AccessorOne.*foobar'):
         foo_bar_baz.test_accessor_one
 
+
 def test_fail_if_missing_keys_two():
     with pytest.raises(AttributeError, match=r'^AccessorTwo.*(foobar|barfoo).*(barfoo|foobar)'):
         foo_bar_baz.test_accessor_two
+
 
 def test_register_method():
     @signal.register_method(AccessorNone, 'foo_method')
     def foo(df):
         return pd.DataFrame({'baz': df['foo'] + df['bar']})
 
-    pd.testing.assert_frame_equal(foo_bar_baz.test_accessor_none.foo_method(), pd.DataFrame({'baz': [2.0]}))
+    accessor = foo_bar_baz.test_accessor_none
+    assert accessor.already_here() == 23
+    pd.testing.assert_frame_equal(accessor.foo_method(), pd.DataFrame({'baz': [2.0]}))
+    assert accessor.some_property == 42
+
+
+def test_getattr_no_method():
+    accessor = foo_bar_baz.test_accessor_none
+    assert accessor.already_here() == 23
+    pd.testing.assert_frame_equal(accessor.foo_method(), pd.DataFrame({'baz': [2.0]}))
+    assert accessor.some_property == 42
 
 
 def test_register_method_fail_duplicate():
