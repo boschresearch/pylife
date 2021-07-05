Fix wrong ND calculation in  Wöhler Curve transformation
Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

pylife/materialdata/woehler/accessors.py
@@ -110,6 +110,7 @@ class WoehlerCurveAccessor(signal.PylifeSignal):
 
         SD = self._obj.SD / 10**((native_ppf-goal_ppf)*scatteringRange2std(self.TS))
         ND = self._obj.ND / 10**((native_ppf-goal_ppf)*scatteringRange2std(self.TN))
+        ND *= np.power(SD/self._obj.SD, -self._obj.k_1)
 
         transformed = self._obj.copy()
         transformed['SD'] = SD
@@ -158,13 +159,11 @@ class WoehlerCurveAccessor(signal.PylifeSignal):
         transformed = self.transform_to_failure_probability(failure_probability)
 
         load, wc = signal.Broadcaster(transformed).broadcast(np.asfarray(load))
-
-        SD_native = np.broadcast_to(self._obj.SD, load.shape)
         cycles = np.full_like(load, np.inf)
 
         k = self._make_k(load, wc.SD)
         in_limit = np.isfinite(k)
-        cycles[in_limit] = wc.ND[in_limit] * np.power(load[in_limit]/SD_native[in_limit], -k[in_limit])
+        cycles[in_limit] = wc.ND[in_limit] * np.power(load[in_limit]/wc.SD[in_limit], -k[in_limit])
 
         return cycles
 
@@ -185,7 +184,6 @@ class WoehlerCurveAccessor(signal.PylifeSignal):
             The cycle numbers at which the component fails for the given `load` values
         """
         transformed = self.transform_to_failure_probability(failure_probability)
-        transformed['ND'] *= np.power(transformed.SD/self._obj.SD, -self._obj.k_1)
 
         cycles, wc = signal.Broadcaster(transformed).broadcast(cycles)
         load = np.asarray(wc.SD.copy())

tests/materialdata/woehler/test_woehler_accessor.py
@@ -42,29 +42,26 @@ def test_woehler_accessor():
 
 
 def test_woehler_transform_probability():
-    wc_10 = pd.Series({
-        'k_1': 7.,
+    wc_50 = pd.Series({
+        'k_1': 2,
         'TS': 1. / 2.,
-        'TN': 1. / 4.,
-        'ND': 1e6,
-        'SD': 300,
-        'failure_probability': 0.1
+        'TN': 1. / 9.,
+        'ND': 3e6,
+        'SD': 300 * np.sqrt(2.),
+        'failure_probability': 0.5
     }).sort_index()
 
-    wc_90 = pd.Series({
-        'k_1': 7.,
-        'TS': 1. / 2.,
-        'TN': 1. / 4.,
-        'ND': 4e6,
-        'SD': 600,
-        'failure_probability': 0.9
-    }).sort_index()
+    transformed_90 = wc_50.woehler.transform_to_failure_probability(0.9)
+    pd.testing.assert_series_equal(transformed_90[['SD', 'ND', 'failure_probability']],
+                                   pd.Series({'SD': 600.0, 'ND': 4.5e6, 'failure_probability': 0.9}))
+    transformed_back = transformed_90.woehler.transform_to_failure_probability(0.5)
+    pd.testing.assert_series_equal(transformed_back, wc_50)
 
-    transformed = wc_10.woehler.transform_to_failure_probability(0.9).sort_index()
-    pd.testing.assert_series_equal(transformed, wc_90)
-
-    transformed = wc_90.woehler.transform_to_failure_probability(0.1).sort_index()
-    pd.testing.assert_series_equal(transformed, wc_10)
+    transformed_10 = wc_50.woehler.transform_to_failure_probability(0.1)
+    pd.testing.assert_series_equal(transformed_10[['SD', 'ND', 'failure_probability']],
+                                   pd.Series({'SD': 300.0, 'ND': 2e6, 'failure_probability': 0.1}))
+    transformed_back = transformed_10.woehler.transform_to_failure_probability(0.5)
+    pd.testing.assert_series_equal(transformed_back, wc_50)
 
 
 def test_woehler_basquin_cycles_50_multiple_load_single_wc():