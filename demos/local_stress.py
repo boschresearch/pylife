ae3be8100dd305ef53341066fa5b320822caa514
Daniel Kreuter <DanielChristopher.Kreuter@de.bosch.com>
2021-08-23 11:15:31 +0200

    Local stress example done
    Signed-off-by: Daniel Kreuter <DanielChristopher.Kreuter@de.bosch.com>

--- demos/local_stress.py
+++ demos/local_stress.py
@@ -7,16 +7,21 @@ Created on Fri Aug 20 12:42:12 2021
 
 import numpy as np
 import pandas as pd
+import pickle
 import pylife.vmap
 import pylife.mesh
 import pylife.mesh.meshsignal
 import pylife.stress.equistress
+import pylife.stress.rainflow
+import pylife.strength.fatigue
 import pyvista as pv
 
+from ansys.dpf import post
+
 pv.set_plot_theme('document')
-pv.set_jupyter_backend('panel')
+pv.set_jupyter_backend('ipyvtklink')
 
-
+import pylife.stress.histogram as psh
 
 
 
@@ -36,56 +41,44 @@ pyLife_mesh = (pylife.vmap.VMAPImport("plate_with_hole.vmap").make_mesh('1', 'ST
                .to_frame())
 
 pyLife_mesh['mises'] = pyLife_mesh.equistress.mises()
-
 grid = pv.UnstructuredGrid(*pyLife_mesh.mesh.vtk_data())
 plotter = pv.Plotter(window_size=[1920, 1080])
 plotter.add_mesh(grid, scalars=pyLife_mesh.groupby('element_id')['mises'].mean().to_numpy(),
                 show_edges=True, cmap='jet')
 plotter.add_scalar_bar()
 plotter.show()
-#%% Ansys (license is necessary)
 
-
+#%% Now we want to apply the collectives to the mesh
 # In[ ]:
-
-
 mises = pyLife_mesh.groupby('element_id')['S11', 'S22', 'S33', 'S12', 'S13', 'S23'].mean().equistress.mises()
-mises /= 200.0  # the nominal load level in the FEM analysis
-#mises
-
+mises /= mises.max()  # the nominal load level in the FEM analysis is set, that s_max = 1
 
 # #### Damage Calculation ####
+collectives = pickle.load(open("collectives.p", "rb"))
+collectives = collectives.unstack().T.fillna(0)
+collectives_sorted = psh.combine_hist([collectives[col] for col in collectives],
+                                             method="sum", nbins=32, histtype="ranges")
 
-# In[ ]:
-
-
-scaled_rainflow = transformed['total'].rainflow.scale(mises)
+scaled_collectives = collectives_sorted.rainflow.scale(mises)
 #scaled_rainflow.amplitude, scaled_rainflow.frequency
 
 
 # In[ ]:
-
-
-damage = mat.fatigue.damage(scaled_rainflow)
-#damage
+mat = pd.Series({
+    'k_1': 8.,
+    'ND': 1.0e6,
+    'SD': 200.0, # range
+    'TN': 1./12.,
+    'TS': 1./1.1
+})
+damage = mat.fatigue.miner_haibach().damage(scaled_collectives)
+print(damage.sum())
 
 
 # In[ ]:
 
 
 damage = damage.groupby(['element_id']).sum()
-#damage
-
-
-# In[ ]:
-
-
-#pyLife_mesh = pyLife_mesh.join(damage)
-#display(pyLife_mesh)
-
-
-# In[ ]:
-
 
 grid = pv.UnstructuredGrid(*pyLife_mesh.mesh.vtk_data())
 plotter = pv.Plotter(window_size=[1920, 1080])
@@ -93,10 +86,30 @@ plotter.add_mesh(grid, scalars=damage.to_numpy(),
                 show_edges=True, cmap='jet')
 plotter.add_scalar_bar()
 plotter.show()
+print("Max damage : %f" % damage.max())
+
+#%% Ansys (license is necessary)
+# For Ansys  *.rst files we are using pymapdl
+# from ansys.mapdl import reader as pymapdl_reader
+# # for more information please go to pymapdl
+# # rst_input = post.load_solution("beam_3d.rst")
+# # # pymapdl has some nice features
+# # rst_input.plot_nodal_displacement(0)
+# # rst_input.plot_nodal_stress(0,"X")
+# ansys_mesh = pymapdl_reader.read_binary('beam_3d.rst')
+# grid_ansys = ansys_mesh.grid
+# plotter = pv.Plotter(window_size=[1920, 1080])
+# _, volume, _  = ansys_mesh.element_solution_data(0,"ENG")
+# volume = pd.DataFrame(volume)[1]
+
+# nodes, ansys_mesh_mises = ansys_mesh.nodal_stress(0)
+# ansys_mesh_mises = pd.DataFrame(data = ansys_mesh_mises,
+#                                 columns=['S11', 'S22', 'S33', 'S12', 'S13', 'S23']).equistress.mises()
 
 
-# In[ ]:
-
-
-print("Maximal damage sum: %f" % damage.max())
-
+# test = pd.DataFrame(ansys_mesh.mesh.elem).iloc[:, 8:]
+# #%%
+# plotter.add_mesh(grid_ansys, scalars=ansys_mesh_mises,
+#                 show_edges=True, cmap='jet')
+# plotter.add_scalar_bar()
+# plotter.show()
