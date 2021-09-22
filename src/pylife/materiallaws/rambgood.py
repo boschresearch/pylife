96617fea1ab22dd98043f6fd122946c4bebb066e
Alexander Maier <maier@wm.tu-darmstadt.de>
2021-09-22 12:58:00 +0200

    handling negative inputs, stress calculation, properties, calculate tangential modulus/compliance
    Signed-off-by: Alexander Maier <maier@wm.tu-darmstadt.de>

--- src/pylife/materiallaws/rambgood.py
+++ src/pylife/materiallaws/rambgood.py
@@ -14,10 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__author__ = "Johannes Mueller"
+__author__ = ["Johannes Mueller", 'Alexander Maier']
 __maintainer__ = __author__
 
 import numpy as np
+from scipy import optimize
 
 
 class RambergOsgood:
@@ -47,6 +48,21 @@ class RambergOsgood:
         self._K = K
         self._n = n
 
+    @property
+    def E(self):
+        '''Get Young's Modulus'''
+        return self._E
+
+    @property
+    def K(self):
+        '''Get the strength coefficient'''
+        return self._K
+
+    @property
+    def n(self):
+        '''Get the strain hardening coefficient'''
+        return self._n
+
     def strain(self, stress):
         '''Calculate the elastic plastic strain for a given stress
 
@@ -60,12 +76,24 @@ class RambergOsgood:
         strain : array-like float
             The resulting strain
 
-        Raises
-        ------
-        ValueError if stress is negative
         '''
         stress = np.asarray(stress)
-        return stress/self._E + self.plastic_strain(stress)
+        return self.elastic_strain(stress) + self.plastic_strain(stress)
+
+    def elastic_strain(self, stress):
+        '''Calculate the elastic strain for a given stress
+
+        Parameters
+        ----------
+        stress : array-like float
+            The stress
+
+        Returns
+        -------
+        strain : array-like float
+            The resulting elastic strain
+        '''
+        return stress/self._E
 
     def plastic_strain(self, stress):
         '''Calculate the plastic strain for a given stress
@@ -79,13 +107,90 @@ class RambergOsgood:
         -------
         strain : array-like float
             The resulting plastic strain
-
-        Raises
-        ------
-        ValueError if stress is negative
         '''
-        self._fail_if_negative(stress)
-        return np.power(stress/self._K, 1./self._n)
+        absstress, signstress = self._get_abs_sign(stress)
+        return signstress * np.power(absstress/self._K, 1./self._n)
+
+    def _get_abs_sign(self, x):
+        '''Calculate the absolute value and the sign for a given input
+        
+        Parameters
+        ----------
+        x : array-like float
+            The input
+
+        Returns
+        -------
+        abs_x : array-like float
+            The resulting absolute value
+        sign_x : array-like float
+            The resulting sign of the input
+        '''
+        abs_x = np.fabs(x)
+        sign_x = np.sign(x)
+        return abs_x, sign_x
+
+    def stress(self, strain, elasticstrain=False, rtol=1e-5, tol=1e-6):
+        '''Calculate the stress for a given strain
+
+        Parameters
+        ----------
+        strain : array-like float
+            The strain
+        elasticstrain (optional): bool
+            If false (default), the given strain is the total strain. If true, the given strain is the elastic strain.
+            
+        Returns
+        -------
+        stress : array-like float
+            The resulting stress
+        '''
+
+        if elasticstrain:
+            return strain * self._E
+
+        def residuum(stress):
+            return self.strain(stress) - abs_strain
+
+        def dresiduum(stress):
+            return self.tangential_compliance(stress)
+
+        strain = np.asarray(strain)
+        abs_strain, sign_strain = self._get_abs_sign(strain)
+        stress0 = self.stress(abs_strain, elasticstrain=True)
+        abs_stress = optimize.newton(func=residuum, x0=stress0, fprime=dresiduum, rtol=rtol, tol=tol)
+        return abs_stress * sign_strain
+
+    def tangential_compliance(self, stress):
+        '''Calculate the derivative of the strain with respect to the stress for a given stress
+
+        Parameters
+        ----------
+        stress : array-like float
+            The stress
+
+        Returns
+        -------
+        dstrain : array-like float
+            The resulting derivative
+        '''
+        stress = np.abs(stress)
+        return 1./self._E + 1./(self._n*self._K) * np.power(stress/self._K, 1./self._n - 1)
+
+    def tangential_modulus(self, stress):
+        '''Calculate the derivative of the stress with respect to the strain for a given stress
+
+        Parameters
+        ----------
+        stress : array-like float
+            The stress
+
+        Returns
+        -------
+        dstress : array-like float
+            The resulting derivative
+        '''
+        return 1. / self.tangential_compliance(stress)
 
     def delta_strain(self, delta_stress):
         '''Calculate the cyclic Masing strain span for a given stress span
@@ -98,19 +203,34 @@ class RambergOsgood:
         Returns
         -------
         delta_strain : array-like float
-            The corresponding stress span
-
-        Raises
-        ------
-        ValueError if delta_stress is negative
+            The corresponding strain span
 
         Notes
         -----
         A Masing like behavior is assumed for the material as described in
         `Kerbgrundkonzept <https://de.wikipedia.org/wiki/Kerbgrundkonzept#Masing-Verhalten_und_Werkstoffged%C3%A4chtnis>`__.
         '''
-        self._fail_if_negative(delta_stress)
-        return delta_stress/self._E + 2.*np.power(delta_stress/(2.*self._K), 1./self._n)
+        return 2*self.strain(stress=delta_stress/2.)
+
+    def delta_stress(self, delta_strain):
+        '''Calculate the cyclic Masing stress span for a given strain span
+
+        Parameters
+        ----------
+        delta_strain : array-like float
+            The strain span
+
+        Returns
+        -------
+        delta_stress : array-like float
+            The corresponding stress span
+
+        Notes
+        -----
+        A Masing like behavior is assumed for the material as described in
+        `Kerbgrundkonzept <https://de.wikipedia.org/wiki/Kerbgrundkonzept#Masing-Verhalten_und_Werkstoffged%C3%A4chtnis>`__.
+        '''
+        return 2*self.stress(strain=delta_strain/2.)
 
     def lower_hysteresis(self, stress, max_stress):
         '''Calculate the lower (relaxation to compression) hysteresis starting from a given maximum stress
@@ -136,6 +256,3 @@ class RambergOsgood:
             raise ValueError("Value for 'stress' must not be higher than 'max_stress'.")
         return self.strain(max_stress) - self.delta_strain(max_stress-stress)
 
-    def _fail_if_negative(self, val):
-        if (np.asarray(val) < 0).any():
-            raise ValueError("Stress value in Ramberg-Osgood equation must not be negative.")
