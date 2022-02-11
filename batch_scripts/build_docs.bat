86c6a567e69f6d196e4f01d49a764e8e2c017f03
Bedey Mate <mate.bedey@hu.bosch.com>
2022-02-11 12:33:19 +0100

    - rem line deleted
    Signed-off-by: Bedey Mate <mate.bedey@hu.bosch.com>

--- batch_scripts/build_docs.bat
+++ batch_scripts/build_docs.bat
@@ -1,7 +1,5 @@
 call conda activate ./.venv
 
-rem call pip install -r docs/requirements.txt
-
 for /f "tokens=*" %%a in ('git rev-parse --show-toplevel') do (set repo_path=%%a)
 
 set jupyter_path=%repo_path%.venv/Scripts/jupyter
