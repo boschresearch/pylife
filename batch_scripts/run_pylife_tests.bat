aad1e736b306d62ef8b25cd58c13c65cf6350e68
Bedey Mate <mate.bedey@hu.bosch.com>
2022-02-11 12:15:18 +0100

    - typo error
    Signed-off-by: Bedey Mate <mate.bedey@hu.bosch.com>

--- batch_scripts/run_pylife_tests.bat
+++ batch_scripts/run_pylife_tests.bat
@@ -4,4 +4,4 @@ call python setup.py build_ext --inplace --force --define CYTHON_TRACE
 
 call python -m pytest -v -ra --cache-clear --junit-xml=junit.xml --cov-report xml:coverage_report.xml --cov-report html:coverage_report --cov=pylife || exit /B 1
 
-call ptest -mdemos
+call pytest -mdemos
