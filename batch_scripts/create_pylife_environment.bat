95d329eba40f24e876fb842e7fb1d22d66d9bad6
Johannes Mueller <johannes.mueller4@de.bosch.com>
2022-02-25 13:34:30 +0100

    Prevent python 3.10 for internal builds for now
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- batch_scripts/create_pylife_environment.bat
+++ batch_scripts/create_pylife_environment.bat
@@ -6,7 +6,7 @@ set jupyter_path=%repo_path%/.venv/Scripts/jupyter
 
 call git config filter.jupyter_clean.clean "%jupyter_path% nbconvert --stdin --stdout --to notebook --ClearOutputPreprocessor.enabled=True"
 
-call conda create -p .venv pip=20.2 pandoc setuptools_scm "python>=3.8"
+call conda create -p .venv pip=20.2 pandoc setuptools_scm "python==3.9"
 
 call conda activate ./.venv
 
