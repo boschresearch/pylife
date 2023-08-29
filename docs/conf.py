0a6e1af6bd7db85ec50e76459113f5f236c534b1
Johannes Mueller <johannes.mueller4@de.bosch.com>
2023-08-29 11:29:09 +0200

    Apply sphinx-rtd-theme and document it as 3rd party artifacts
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>
    Signed-off-by: Benjamin Maier <Benjamin.Maier2@de.bosch.com>

--- docs/conf.py
+++ docs/conf.py
@@ -193,7 +193,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
