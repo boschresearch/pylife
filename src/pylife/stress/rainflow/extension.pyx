20b0e0cc6abb3dd57715e4c5767e93885b9f5f15
Johannes Mueller <johannes.mueller4@de.bosch.com>
2024-04-25 15:28:08 +0200

    Make indeces in cython unsigned long
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- src/pylife/stress/rainflow/extension.pyx
+++ src/pylife/stress/rainflow/extension.pyx
@@ -6,7 +6,7 @@ cimport cython
 
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
-def fourpoint_loop(double [::1] turns, long [::1] turns_index):
+def fourpoint_loop(double [::1] turns, unsigned long [::1] turns_index):
     cdef Py_ssize_t len_turns = len(turns)
 
     from_vals = np.empty(len_turns//2, dtype=np.float64)
@@ -16,19 +16,19 @@ def fourpoint_loop(double [::1] turns, long [::1] turns_index):
 
     cdef double [::1] from_vals_v = from_vals
     cdef double [::1] to_vals_v = to_vals
-    cdef long [::1] from_index_v = from_index
-    cdef long [::1] to_index_v = to_index
+    cdef unsigned long [::1] from_index_v = from_index
+    cdef unsigned long [::1] to_index_v = to_index
 
     residual_index = np.empty(len_turns, dtype=np.int64)
 
-    cdef long [::1] residual_index_v = residual_index
+    cdef unsigned long [::1] residual_index_v = residual_index
 
     residual_index_v[0] = 0
     residual_index_v[1] = 1
 
-    cdef long i = 2
-    cdef long ii = 2
-    cdef long t = 0
+    cdef unsigned long i = 2
+    cdef unsigned long ii = 2
+    cdef unsigned long t = 0
 
     cdef double a
     cdef double b
