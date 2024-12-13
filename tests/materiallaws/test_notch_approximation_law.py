4c648a0d55e204f7431bd036269356ba046429e1
Johannes Mueller <johannes.mueller4@de.bosch.com>
2024-12-13 10:33:53 +0100

    Drop Binned class for notch approximation and adjust tests accordingly
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- tests/materiallaws/test_notch_approximation_law.py
+++ tests/materiallaws/test_notch_approximation_law.py
@@ -19,105 +19,45 @@ __maintainer__ = __author__
 
 import pytest
 import numpy as np
-import pandas as pd
-
-import pylife.strength.damage_parameter
 from pylife.materiallaws.notch_approximation_law import ExtendedNeuber, NotchApproxBinner
 from .data import *
 
-def test_extended_neuber_example_1():
+
+@pytest.mark.parametrize('load, strain, stress', [
+    (3.592, 0.0017e-2, 3.592),
+    (7.185, 0.0035e-2, 7.185),
+    (176.057, 8.71e-4, 172.639),
+    (359.3, 0.0021, 299.78)
+])
+def test_extended_neuber_example_1(load, strain, stress):
     """ example under 2.7.1, p.74 of FKM nonlinear "Akademisches Beispiel" """
+    notch_approximation_law = ExtendedNeuber(E=206e3, K=1184, n=0.187, K_p=3.5)
 
-    E = 206e3    # [MPa] Young's modulus
-    K = 1184     # [MPa]
-    n = 0.187    # [-]
-    K_p = 3.5    # [-] (de: Traglastformzahl) K_p = F_plastic / F_yield (3.1.1)
-
-    L = pd.Series([100, -200, 100, -250, 200, 0, 200, -200])
-    c = 1.4
-    gamma_L = (250+6.6)/250
-    L = c * gamma_L * L
-
-    # initialize notch approximation law and damage parameter
-    notch_approximation_law = ExtendedNeuber(E, K, n, K_p)
-
-    assert notch_approximation_law.E == E
-    assert notch_approximation_law.K == K
-    assert notch_approximation_law.n == n
-    assert notch_approximation_law.K_p == K_p
-
-    maximum_absolute_load = max(abs(L))
-
-    # the FKM example seems to round here, real value is 359.24
-    assert np.isclose(maximum_absolute_load, 359.3, rtol=1e-3)
-
-    binned_notch_approximation_law = pylife.materiallaws.notch_approximation_law.Binned(
-        notch_approximation_law, maximum_absolute_load, 100)
-
-    # some rows of PFAD are given in the FKM nonlinear example on p.76
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[0], \
-        pd.Series([3.592, 0.0017e-2, 3.592]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[1], \
-        pd.Series([7.185, 0.0035e-2, 7.185]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # Note that the original FKM document has an error at this row (it is row 49, not 50)
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[48], \
-        pd.Series([176.057, 8.71e-4, 172.639]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[99], \
-        pd.Series([359.3, 0.0021, 299.78]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # matrix AST on page 162, chapter 3.4.1
-    pd.testing.assert_frame_equal(
-        binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_162, rtol=1e-3, atol=1e-5)
-
-    assert np.isclose(binned_notch_approximation_law._lut_primary_branch.load.max(), maximum_absolute_load)
+    assert np.isclose(notch_approximation_law.stress(load), stress, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.strain(stress, None), strain, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.load(stress), load, rtol=1e-1)
 
 
-def test_extended_neuber_example_2():
+@pytest.mark.parametrize('load, strain, stress', [
+    (10.13, 0.0049e-2, 10.130),
+    (20.26, 0.0098e-2, 20.260),
+    (1013.00, 0.6035e-2, 829.681),
+])
+def test_extended_neuber_example_2(load, strain, stress):
     """ example under 2.7.2, p.78 of FKM nonlinear, "Welle mit V-Kerbe" """
+    notch_approximation_law = ExtendedNeuber(E=206e3, K=2650.5, n=0.187, K_p=3.5)
 
-    E = 206e3    # [MPa] Young's modulus
-    K = 2650.5   # [MPa]
-    n = 0.187    # [-]
-    K_p = 3.5    # [-] (de: Traglastformzahl) K_p = F_plastic / F_yield (3.1.1)
+    assert np.isclose(notch_approximation_law.stress(load), stress, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.strain(stress, None), strain, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.load(stress), load, rtol=1e-1)
 
-    L = 1266.25 * pd.Series([0.3, -0.3, 0.5, -0.5, 0.6, -0.6, 0.3, -0.3, 0.7, -0.7, 0.2, -0.2, 0.6, -0.6, 0.8, -0.8, 0.8, -0.8])
-
-    # initialize notch approximation law and damage parameter
-    notch_approximation_law = ExtendedNeuber(E, K, n, K_p)
-
-    maximum_absolute_load = max(abs(L))
-
-    # the FKM example seems to round here, real value is 359.24
-    assert np.isclose(maximum_absolute_load, 1013, rtol=1e-3)
-
-    binned_notch_approximation_law = pylife.materiallaws.notch_approximation_law.Binned(
-        notch_approximation_law, maximum_absolute_load)
-
-    # some rows of PFAD are given in the FKM nonlinear example on p.79
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[0], \
-        pd.Series([10.13, 0.0049e-2, 10.130]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[1], \
-        pd.Series([20.26, 0.0098e-2, 20.260]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # this row seems off in the FKM nonlinear example, error is as high as 5%
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[99], \
-        pd.Series([1013.00, 0.6035e-2, 829.681]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # test binning directly
-    assert np.isclose(binned_notch_approximation_law.stress(10.13), 10.130)
-    assert np.isclose(binned_notch_approximation_law.strain(10.30, 10.13), 0.0049174e-2)
-
-    assert np.isclose(binned_notch_approximation_law.stress_secondary_branch(10.13), 10.130)
-    assert np.isclose(binned_notch_approximation_law.strain_secondary_branch(10.30, 10.13), 0.0049174e-2)
+    assert np.isclose(notch_approximation_law.stress_secondary_branch(10.13), 10.130)
+    assert np.isclose(notch_approximation_law.strain_secondary_branch(10.30, 10.13), 0.0049174e-2, rtol=1e-1)
 
     # matrix AST on page 171, chapter 3.4.2
 
-    pd.testing.assert_frame_equal(
-        binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_171, rtol=1e-3, atol=1e-5)
+    # pd.testing.assert_frame_equal(
+    #     binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_171, rtol=1e-3, atol=1e-5)
 
 
 def test_extended_neuber_example_no_binning_vectorized():
--- tests/materiallaws/test_notch_approximation_law_seegerbeste.py
+++ tests/materiallaws/test_notch_approximation_law_seegerbeste.py
@@ -26,91 +26,45 @@ import pylife.materiallaws.notch_approximation_law
 from pylife.materiallaws.notch_approximation_law_seegerbeste import SeegerBeste
 from .data import *
 
-
-def test_seeger_beste_example_1():
+@pytest.mark.parametrize('load, strain, stress', [
+    (3.592, 0.0017e-2, 3.592),
+    (7.185, 0.0035e-2, 7.185),
+    (176.057, 8.71e-4, 172.639),
+    (359.3, 0.0019, 283.86)
+])
+def test_seeger_beste_example_1(load, strain, stress):
     """ example under 2.7.1, p.74 of FKM nonlinear "Akademisches Beispiel" """
+    notch_approximation_law = SeegerBeste(E=206e3, K=1184, n=0.187, K_p=3.5)
 
-    E = 206e3    # [MPa] Young's modulus
-    K = 1184     # [MPa]
-    n = 0.187    # [-]
-    K_p = 3.5    # [-] (de: Traglastformzahl) K_p = F_plastic / F_yield (3.1.1)
+    assert np.isclose(notch_approximation_law.stress(load), stress, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.strain(stress, None), strain, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.load(stress), load, rtol=1e-1)
 
-    L = pd.Series([100, -200, 100, -250, 200, 0, 200, -200])
-    c = 1.4
-    gamma_L = (250+6.6)/250
-    L = c * gamma_L * L
-
-    # initialize notch approximation law and damage parameter
-    #notch_approximation_law = ExtendedNeuber(E, K, n, K_p)
-    notch_approximation_law = SeegerBeste(E, K, n, K_p)
-#    damage_parameter_type = pylife.strength.damage_parameter.P_RAM()
-
-    maximum_absolute_load = max(abs(L))
-#    print(maximum_absolute_load)
-
-    # the FKM example seems to round here, real value is 359.24
-    assert np.isclose(maximum_absolute_load, 359.3, rtol=1e-3)
-
-    binned_notch_approximation_law = pylife.materiallaws.notch_approximation_law.Binned(notch_approximation_law, maximum_absolute_load)
-
-    # some rows of PFAD are given in the FKM nonlinear example on p.76
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[0], \
-        pd.Series([3.59, 0.0017e-2, 3.59]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[1], \
-        pd.Series([7.18, 0.0035e-2, 7.18]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # this row seems off in the FKM nonlinear example, error is as high as 5%
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[49], \
-        #pd.Series([179.62, 8.73e-4, 172.93]), check_names=False, check_index=False, rtol=10e-2, atol=1e-5)
-        pd.Series([179.62, 8.73e-4, 172.93]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[99], \
-        pd.Series([359.24, 0.001859, 283.86]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
 
     # matrix AST on page 162, chapter 3.4.1
-    pd.testing.assert_frame_equal(
-        binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_162_seeger_beste, rtol=1e-3, atol=1e-5)
+    # pd.testing.assert_frame_equal(
+    #     binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_162_seeger_beste, rtol=1e-3, atol=1e-5)
 
-    assert np.isclose(binned_notch_approximation_law._lut_primary_branch.load.max(), maximum_absolute_load)
+    # assert np.isclose(binned_notch_approximation_law._lut_primary_branch.load.max(), maximum_absolute_load)
 
 
-def test_seeger_beste_example_2():
+@pytest.mark.parametrize('load, strain, stress', [
+    (10.13, 0.0049e-2, 10.130),
+    (20.26, 0.0098e-2, 20.260),
+    (1013.00, 0.53e-2, 784.89),
+])
+def test_seeger_beste_example_2(load, strain, stress):
     """ example under 2.7.2, p.78 of FKM nonlinear, "Welle mit V-Kerbe" """
+    notch_approximation_law = SeegerBeste(E=206e3, K=2650.5, n=0.187, K_p=3.5)
 
-    E = 206e3    # [MPa] Young's modulus
-    K = 2650.5   # [MPa]
-    n = 0.187    # [-]
-    K_p = 3.5    # [-] (de: Traglastformzahl) K_p = F_plastic / F_yield (3.1.1)
-
-    L = 1266.25 * pd.Series([0.3, -0.3, 0.5, -0.5, 0.6, -0.6, 0.3, -0.3, 0.7, -0.7, 0.2, -0.2, 0.6, -0.6, 0.8, -0.8, 0.8, -0.8])
-
-    # initialize notch approximation law and damage parameter
-    notch_approximation_law = SeegerBeste(E, K, n, K_p)
-
-    maximum_absolute_load = max(abs(L))
-
-    # the FKM example seems to round here, real value is 359.24
-    assert np.isclose(maximum_absolute_load, 1013, rtol=1e-3)
-
-    binned_notch_approximation_law = pylife.materiallaws.notch_approximation_law.Binned(
-        notch_approximation_law, maximum_absolute_load)
-
-    # some rows of PFAD are given in the FKM nonlinear example on p.79
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[0], \
-        pd.Series([10.13, 0.0049e-2, 10.130]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[1], \
-        pd.Series([20.26, 0.01e-2, 20.260]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
-
-    # this row seems off in the FKM nonlinear example, error is as high as 5%
-    pd.testing.assert_series_equal(binned_notch_approximation_law._lut_primary_branch.iloc[99], \
-        pd.Series([1013.00, 0.53e-2, 784.890]), check_names=False, check_index=False, rtol=1e-3, atol=1e-5)
+    assert np.isclose(notch_approximation_law.stress(load), stress, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.strain(stress, None), strain, rtol=1e-1)
+    assert np.isclose(notch_approximation_law.load(stress), load, rtol=1e-1)
 
 
-    # matrix AST on page 171, chapter 3.4.2
-    pd.testing.assert_frame_equal(
-        binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_171_seeger_beste, rtol=2e-3, atol=1e-5)
+    # # matrix AST on page 171, chapter 3.4.2
+    # pd.testing.assert_frame_equal(
+    #     binned_notch_approximation_law._lut_secondary_branch, expected_matrix_AST_171_seeger_beste, rtol=2e-3, atol=1e-5)
 
 
 def test_seeger_beste_example_no_binning():
