03e4e8fee8024e22b8e957b17efc9e16e6d6834c
Johannes Mueller <johannes.mueller4@de.bosch.com>
2025-10-21 11:20:54 +0200

    Try the newest Abaqus first on Windows
    Signed-off-by: Johannes Mueller <johannes.mueller4@de.bosch.com>

--- tools/odbclient/src/odbclient/odbclient.py
+++ tools/odbclient/src/odbclient/odbclient.py
@@ -575,13 +575,13 @@ def _guess_abaqus_bin():
 
 def _guess_abaqus_bin_windows():
     guesses = [
-        r"C:/Program Files/SIMULIA/2018/AbaqusCAE/win_b64/code/bin/ABQLauncher.exe",
-        r"C:/Program Files/SIMULIA/2020/EstProducts/win_b64/code/bin/ABQLauncher.exe",
-        r"C:/Program Files/SIMULIA/2020/Products/win_b64/code/bin/ABQLauncher.exe",
-        r"C:/Program Files/SIMULIA/2021/EstProducts/win_b64/code/bin/ABQLauncher.exe",
-        r"C:/Program Files/SIMULIA/2022/EstProducts/win_b64/code/bin/SMALauncher.exe",
-        r"C:/Program Files/SIMULIA/2023/EstProducts/win_b64/code/bin/SMALauncher.exe",
         r"C:/Program Files/SIMULIA/2024/EstProducts/win_b64/code/bin/SMALauncher.exe",
+        r"C:/Program Files/SIMULIA/2023/EstProducts/win_b64/code/bin/SMALauncher.exe",
+        r"C:/Program Files/SIMULIA/2022/EstProducts/win_b64/code/bin/SMALauncher.exe",
+        r"C:/Program Files/SIMULIA/2021/EstProducts/win_b64/code/bin/ABQLauncher.exe",
+        r"C:/Program Files/SIMULIA/2020/Products/win_b64/code/bin/ABQLauncher.exe",
+        r"C:/Program Files/SIMULIA/2020/EstProducts/win_b64/code/bin/ABQLauncher.exe",
+        r"C:/Program Files/SIMULIA/2018/AbaqusCAE/win_b64/code/bin/ABQLauncher.exe",
     ]
     for guess in guesses:
         if os.path.exists(guess):
