e3b233c90233e52440609f0de5a0ea5fe01a528a
feegeer <85125437+feegeer@users.noreply.github.com>
2026-01-09 13:02:37 +0100

    Added edge cases for polished surfaces
    Signed-off-by: feegeer <85125437+feegeer@users.noreply.github.com>

--- tests/strength/test_parameter_calculations.py
+++ tests/strength/test_parameter_calculations.py
@@ -189,45 +189,52 @@ def test_calculate_roughness_parameter_K_RP_already_given(capsys):
     assert np.isclose(result["K_RP"], 0.95)
     assert capsys.readouterr().out == "The parameter `K_RP` is already set to 0.95, not using the FKM formula.\n"
 
-def test_calculate_roughness_material_woehler_parameters_P_RAM():
+@pytest.mark.parametrize("K_RP", [0.8468, 1.0])
+def test_calculate_roughness_material_woehler_parameters_P_RAM(K_RP):
     assessment_parameters = pd.Series({
         "P_RAM_Z_WS": 819.00,
         "P_RAM_D_WS": 335.02,
         "d_2": -0.197,
-        "K_RP": 0.8468470008671309,
+        "K_RP": K_RP,
     })
 
     result = pylife.strength.fkm_nonlinear.parameter_calculations.\
         calculate_roughness_material_woehler_parameters_P_RAM(assessment_parameters)
 
-    assert np.isclose(result["P_RAM_D_WS_rau"], 335.02 * 0.8468470008671309)
+    assert np.isclose(result["P_RAM_D_WS_rau"], assessment_parameters["P_RAM_D_WS"] * assessment_parameters["K_RP"])
 
-    N_D = 1e3 * (335.02 / 819.00) ** (1 / -0.197)
-    d2_alt = np.log((335.02 * 0.8468470008671309) / 819.00) / np.log(N_D / 1e3)
-    assert np.isclose(result["d2_RAM_rau"], d2_alt, rtol=1e-12, atol=0.0)
+    if K_RP < 1.0:
+        N_D = 1e3 * (assessment_parameters["P_RAM_D_WS"] / assessment_parameters["P_RAM_Z_WS"]) ** (1 / assessment_parameters["d_2"])
+        d2_alt = np.log((assessment_parameters["P_RAM_D_WS"] * assessment_parameters["K_RP"]) / assessment_parameters["P_RAM_Z_WS"]) / np.log(N_D / 1e3)
+        assert np.isclose(result["d2_RAM_rau"], d2_alt, rtol=1e-12, atol=0.0)
+    else:
+        assert np.isclose(result["d2_RAM_rau"], assessment_parameters["d_2"], rtol=1e-12, atol=0.0)
 
 
-def test_calculate_roughness_material_woehler_parameters_P_RAJ():
+@pytest.mark.parametrize("K_RP", [0.8468, 1.0])
+def test_calculate_roughness_material_woehler_parameters_P_RAJ(K_RP):
     assessment_parameters = pd.Series({
         "P_RAJ_Z_WS": 433.67,
         "P_RAJ_D_WS": 0.0897,
         "d_RAJ": -0.63,
-        "K_RP": 0.90,
+        "K_RP": K_RP,
     })
 
     result = pylife.strength.fkm_nonlinear.parameter_calculations.\
         calculate_roughness_material_woehler_parameters_P_RAJ(assessment_parameters)
+    expected_P_RAJ_Z_1e3 = assessment_parameters["P_RAJ_Z_WS"] * np.power(1e3, assessment_parameters["d_RAJ"])
 
-    expected_P_RAJ_Z_1e3 = assessment_parameters["P_RAJ_Z_WS"] * assessment_parameters["d_RAJ"] ** 1e3
     assert np.isclose(result["P_RAJ_Z_1e3"], expected_P_RAJ_Z_1e3)
-    assert np.isclose(result["P_RAJ_D_WS_rau"], assessment_parameters["P_RAJ_D_WS"] * assessment_parameters["K_RP"]**2)
+    assert np.isclose(result["P_RAJ_D_WS_rau"], assessment_parameters["P_RAJ_D_WS"] * assessment_parameters["K_RP"] ** 2)
 
-    N_D = (assessment_parameters["P_RAJ_D_WS"] / assessment_parameters["P_RAJ_Z_WS"]) ** (1/assessment_parameters["d_RAJ"])
-    d2_alt = np.log(result["P_RAJ_D_WS_rau"] / result["P_RAJ_Z_1e3"]) / np.log(N_D/1e3)
-    assert np.isclose(result["d_RAJ_2_rau"], d2_alt, rtol=1e-12, atol=0.0)
+    if K_RP < 1.0:
+        N_D = (assessment_parameters["P_RAJ_D_WS"] / assessment_parameters["P_RAJ_Z_WS"]) ** (1/assessment_parameters["d_RAJ"])
+        d2_alt = np.log(result["P_RAJ_D_WS_rau"] / result["P_RAJ_Z_1e3"]) / np.log(N_D/1e3)
+        assert np.isclose(result["d_RAJ_2_rau"], d2_alt, rtol=1e-12, atol=0.0)
+    else:
+         assert np.isclose(result["d_RAJ_2_rau"],assessment_parameters["d_RAJ"], rtol=1e-12, atol=0.0)
 
-
-@pytest.mark.parametrize("include_n_P, n_P", [(True, 1.3), (False, 1.3)])
+@pytest.mark.parametrize("include_n_P, n_P", [(True, 1.3), (False, 1.0)])
 def test_calculate_roughness_component_woehler_parameters_P_RAM(include_n_P, n_P):
     assessment_parameters = pd.Series({
         "gamma_M_RAM": 1.2,
@@ -251,7 +258,7 @@ def test_calculate_roughness_component_woehler_parameters_P_RAJ(include_n_P, n_P
         "gamma_M_RAJ": 1.3,
         "n_P": n_P,
         "P_RAJ_Z_WS": 433.67,
-        "P_RAJ_D_WS_rau": 0.095,
+        "P_RAJ_D_WS_rau": 0.0897,
         "P_RAJ_Z_1e3": 433.67 * np.power(1e3, -0.63)
     })
 
